f2f27f14c37024e2886b587f61bd4529e83e19d1
Sam Procter <sprocter@sei.cmu.edu>
2016-11-07 11:26:51 -0500

    Update standalone code to be ATSV compatible

--- org.osate.atsv.integration/src/main/java/org/osate/atsv/standalone/NetworkHandler.java
+++ org.osate.atsv.integration/src/main/java/org/osate/atsv/standalone/NetworkHandler.java
@@ -3,6 +3,7 @@ package org.osate.atsv.standalone;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Map.Entry;
 
@@ -23,35 +24,35 @@ public class NetworkHandler {
 	public static void main(String[] args) {
 		String host = args[0];
 		int port = Integer.parseInt(args[1]);
-		try (Socket socket = new Socket(host, port)) {
+		try (Socket socket = new Socket(host, port); PrintWriter out = new PrintWriter("output.txt")) {
 			initializeSender(socket);
 			initializeListener(socket); // We could initialize a listener per request, to parallelize things
 			sendRequest(getMockRequest());
 			Response r;
-			while((r = (Response) inStream.readObject()) != null){
-				if(r.hasException()){
-					System.err.println("An exception occurred.");
-					System.err.println(r.getException().getMessage());
-					r.getException().printStackTrace(System.err);
-					continue;
+			r = (Response) inStream.readObject();
+			if (r.hasException()) {
+				System.err.println("An exception occurred.");
+				System.err.println(r.getException().getMessage());
+				r.getException().printStackTrace(System.err);
+			} else {
+				for (Entry<String, String> e : r.getVariables().entrySet()) {
+					out.println(e.getKey() + "," + e.getValue());
 				}
-				for(Entry<String, String> e : r.getVariables().entrySet()){
-					System.out.println(e.getKey() + " -- " + e.getValue());
-				}
+				out.flush();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private static Request getMockRequest(){
+
+	private static Request getMockRequest() {
 		Request r = new Request();
 		r.setPluginId("org.osate.atsv.integration.flow-latency");
 		r.setPackageName("CarSystem");
 		r.setComponentImplementationName("MyCar.CruiseControlTasks");
 		return r;
 	}
-	
+
 	private static void initializeListener(Socket socket) throws IOException {
 		inStream = new ObjectInputStream(socket.getInputStream());
 	}
@@ -60,7 +61,7 @@ public class NetworkHandler {
 		outStream = new ObjectOutputStream(socket.getOutputStream());
 	}
 
-	public static void sendRequest(Request r) throws IOException{
+	public static void sendRequest(Request r) throws IOException {
 		outStream.writeObject(r);
 		outStream.flush();
 	}
