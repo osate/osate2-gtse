b0692481be028383618bc69b7bdb8f9c60be6781
Sam Procter <sprocter@sei.cmu.edu>
2017-07-12 14:04:49 -0400

    Add exception for unused features

---
+++ org.osate.atsv.integration/src/main/java/org/osate/atsv/integration/exception/UnsupportedFeatureException.java
@@ -0,0 +1,13 @@
+package org.osate.atsv.integration.exception;
+
+public class UnsupportedFeatureException extends Exception {
+
+	/**
+	 * Default serial id
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UnsupportedFeatureException(String msg) {
+		super(msg);
+	}
+}
